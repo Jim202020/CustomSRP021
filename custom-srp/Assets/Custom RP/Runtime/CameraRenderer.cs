b94aede586d3d11edcf7b41f9089d1397fe15fb0
lijun <lijun@sl916.com>
2022-06-30 23:08:39 +0800

    【Custom Render Pipeline】2.2 Drawing the Skybox

--- custom-srp/Assets/Custom RP/Runtime/CameraRenderer.cs
+++ custom-srp/Assets/Custom RP/Runtime/CameraRenderer.cs
@@ -10,5 +10,21 @@ public class CameraRenderer {
 	public void Render (ScriptableRenderContext context, Camera camera) {
 		this.context = context;
 		this.camera = camera;
+
+        Setup();
+		DrawVisibleGeometry();
+		Submit();
+	}
+
+    void Setup () {
+		context.SetupCameraProperties(camera);
+	}
+
+    void DrawVisibleGeometry () {
+		context.DrawSkybox(camera);
+	}
+
+	void Submit () {
+		context.Submit();
 	}
 }
