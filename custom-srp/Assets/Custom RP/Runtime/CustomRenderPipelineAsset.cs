603f8c84449720c4caefad158343ecb8c2747f82
lijun <lijun@sl916.com>
2022-06-30 23:00:24 +0800

    【Custom Render Pipeline】1.3 Render Pipeline Instance

--- custom-srp/Assets/Custom RP/Runtime/CustomRenderPipelineAsset.cs
+++ custom-srp/Assets/Custom RP/Runtime/CustomRenderPipelineAsset.cs
@@ -4,6 +4,6 @@ using UnityEngine.Rendering;
 [CreateAssetMenu(menuName = "Rendering/Custom Render Pipeline")]
 public class CustomRenderPipelineAsset : RenderPipelineAsset {
     protected override RenderPipeline CreatePipeline () {
-		return null;
+		return new CustomRenderPipeline();
 	}
 }
