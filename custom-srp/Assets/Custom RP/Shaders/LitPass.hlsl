9fc945cb395eb0d11bffdd9b00211c9341421e64
lijun <lijun@sl916.com>
2022-07-06 22:20:54 +0800

    【Directional Lights】1.2 Normal Vectors

--- custom-srp/Assets/Custom RP/Shaders/LitPass.hlsl
+++ custom-srp/Assets/Custom RP/Shaders/LitPass.hlsl
@@ -14,12 +14,14 @@ UNITY_INSTANCING_BUFFER_END(UnityPerMaterial)
 
 struct Attributes {
 	float3 positionOS : POSITION;
+	float3 normalOS : NORMAL;
 	float2 baseUV : TEXCOORD0;
 	UNITY_VERTEX_INPUT_INSTANCE_ID
 };
 
 struct Varyings {
 	float4 positionCS : SV_POSITION;
+	float3 normalWS : VAR_NORMAL;
 	float2 baseUV : VAR_BASE_UV;
 	UNITY_VERTEX_INPUT_INSTANCE_ID
 };
@@ -30,6 +32,7 @@ Varyings LitPassVertex (Attributes input) {
 	UNITY_TRANSFER_INSTANCE_ID(input, output);
 	float3 positionWS = TransformObjectToWorld(input.positionOS);
 	output.positionCS = TransformWorldToHClip(positionWS);
+	output.normalWS = TransformObjectToWorldNormal(input.normalOS);
 
 	float4 baseST = UNITY_ACCESS_INSTANCED_PROP(UnityPerMaterial, _BaseMap_ST);
 	output.baseUV = input.baseUV * baseST.xy + baseST.zw;
