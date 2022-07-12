51f1843cd01700ceb59ba54cb91467e8a44f8e9c
lijun <lijun@sl916.com>
2022-07-12 21:35:11 +0800

    【Directional Lights】3.5 Reflectivity 3.6 Specular Color 3.7 Roughness

--- custom-srp/Assets/Custom RP/ShaderLibrary/BRDF.hlsl
+++ custom-srp/Assets/Custom RP/ShaderLibrary/BRDF.hlsl
@@ -7,11 +7,21 @@ struct BRDF {
 	float roughness;
 };
 
+#define MIN_REFLECTIVITY 0.04
+
+float OneMinusReflectivity (float metallic) {
+	float range = 1.0 - MIN_REFLECTIVITY;
+	return range - metallic * range;
+}
+
 BRDF GetBRDF (Surface surface) {
 	BRDF brdf;
-	brdf.diffuse = surface.color;
-	brdf.specular = 0.0;
-	brdf.roughness = 1.0;
+	float oneMinusReflectivity = OneMinusReflectivity(surface.metallic);
+	brdf.diffuse = surface.color * oneMinusReflectivity;
+	brdf.specular = lerp(MIN_REFLECTIVITY, surface.color, surface.metallic);
+	float perceptualRoughness =
+		PerceptualSmoothnessToPerceptualRoughness(surface.smoothness);
+	brdf.roughness = PerceptualRoughnessToRoughness(perceptualRoughness);
 	return brdf;
 }
 
