338af98e86bb7fda8fb0b777723ba411857ea819
lijun <lijun@sl916.com>
2022-07-12 22:21:11 +0800

    【Directional Lights】3.10 Mesh Ball

--- custom-srp/Assets/Custom RP/Examples/MeshBall.cs
+++ custom-srp/Assets/Custom RP/Examples/MeshBall.cs
@@ -2,7 +2,14 @@
 
 public class MeshBall : MonoBehaviour {
 
-	static int baseColorId = Shader.PropertyToID("_BaseColor");
+	static int
+		baseColorId = Shader.PropertyToID("_BaseColor"),
+		metallicId = Shader.PropertyToID("_Metallic"),
+		smoothnessId = Shader.PropertyToID("_Smoothness");
+
+	float[]
+		metallic = new float[1023],
+		smoothness = new float[1023];
 
 	[SerializeField]
 	Mesh mesh = default;
@@ -29,6 +36,8 @@ public class MeshBall : MonoBehaviour {
 					Random.value, Random.value, Random.value,
 					Random.Range(0.5f, 1f)
 				);
+			metallic[i] = Random.value < 0.25f ? 1f : 0f;
+			smoothness[i] = Random.Range(0.05f, 0.95f);
 		}
 	}
 
@@ -36,6 +45,8 @@ public class MeshBall : MonoBehaviour {
 		if (block == null) {
 			block = new MaterialPropertyBlock();
 			block.SetVectorArray(baseColorId, baseColors);
+			block.SetFloatArray(metallicId, metallic);
+			block.SetFloatArray(smoothnessId, smoothness);
 		}
 		Graphics.DrawMeshInstanced(mesh, 0, material, matrices, 1023, block);
 	}
