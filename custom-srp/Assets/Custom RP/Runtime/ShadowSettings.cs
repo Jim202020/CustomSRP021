368075f9160329c28b6c8f16db47062b9e7f7a0c
lijun <lijun@sl916.com>
2022-07-20 23:02:16 +0800

    【Directional Shadows】【3 Cascaded Shadow Maps】3.1 Settings

--- custom-srp/Assets/Custom RP/Runtime/ShadowSettings.cs
+++ custom-srp/Assets/Custom RP/Runtime/ShadowSettings.cs
@@ -6,7 +6,7 @@ public class ShadowSettings {
 	[Min(0f)]
 	public float maxDistance = 100f;
 
-    public enum TextureSize {
+    public enum MapSize {
 		_256 = 256, _512 = 512, _1024 = 1024,
 		_2048 = 2048, _4096 = 4096, _8192 = 8192
 	}
@@ -14,10 +14,23 @@ public class ShadowSettings {
     [System.Serializable]
 	public struct Directional {
 
-		public TextureSize atlasSize;
+		public MapSize atlasSize;
+
+		[Range(1, 4)]
+		public int cascadeCount;
+
+		[Range(0f, 1f)]
+		public float cascadeRatio1, cascadeRatio2, cascadeRatio3;
+
+		public Vector3 CascadeRatios =>
+			new Vector3(cascadeRatio1, cascadeRatio2, cascadeRatio3);
 	}
 
 	public Directional directional = new Directional {
-		atlasSize = TextureSize._1024
+		atlasSize = MapSize._1024,
+		cascadeCount = 4,
+		cascadeRatio1 = 0.1f,
+		cascadeRatio2 = 0.25f,
+		cascadeRatio3 = 0.5f
 	};
 }
